93f05f761127f289561d931f2534f32b54e4fc26
cam <cameron.l.feenstra@gmail.com>
2018-06-04 15:31:53 -0500

    fixed typo

--- pyvote/utils.py
+++ pyvote/utils.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 def rowwise_indexed(mat, idx):
-	ran = np.arange(mat.shape[0]).reshape(-1, 1)
-	ran = np.repeat(ran, mat.shape[1], axis=1)
+	ran = np.arange(idx.shape[0]).reshape(-1, 1)
+	ran = np.repeat(ran, idx.shape[1], axis=1)
 	return mat[ran, idx]
 
 def columnwise_indexed(mat, idx):
-	ran = np.arange(mat.shape[1]).reshape(1, -1)
-	ran = np.repeat(ran, mat.shape[0])
+	ran = np.arange(idx.shape[1]).reshape(1, -1)
+	ran = np.repeat(ran, idx.shape[0])
 	return mat[ran, idx]
