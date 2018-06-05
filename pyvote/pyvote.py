2f9eef18bff29e3f02c938375ae14dc3ceb4b521
cfeenstra67 <cameron.l.feenstra@gmail.com>
2018-06-04 21:39:40 -0500

    added support for class substitution when using return_means=True for Prediction s

--- pyvote/pyvote.py
+++ pyvote/pyvote.py
@@ -1,15 +1,17 @@
 from operator import itemgetter
 from collections import Counter
 import numpy as np 
+from operator import attrgetter
 from .plugins import get_plugin, BasePlugin
-from .utils import rowwise_indexed
+from .utils import rowwise_indexed, row_conv_to_idx
 
 class Predictions(object):
 
-    def __init__(self, pred_mat, probs, voter):
+    def __init__(self, pred_mat, probs, conv_mat, voter):
         self.pred_mat = pred_mat
         self.probs = probs
         self.voter = voter
+        self.conv_mat = conv_mat
 
     def tally_votes(self, class_type='binary', top_classes=5, min_votes=1, return_means=False):
         pred_mat = self.pred_mat
@@ -43,7 +45,8 @@ class Predictions(object):
             return votes_mat
 
         avg_prob = self.probs.mean(axis=1)
-        return votes_mat, rowwise_indexed(avg_prob, votes_mat)
+        ind_mat = row_conv_to_idx(votes_mat, self.conv_mat)
+        return votes_mat, rowwise_indexed(avg_prob, ind_mat)
 
 
 class ModelVote(object):
@@ -65,6 +68,7 @@ class ModelVote(object):
         # matrix shape (n_samples, n_models, n_classes, 2)
         all_votes = []
         all_probs = []
+        all_convs = []
         for i, model in enumerate(self.models):
             model_data = self.datagetter[i](data)
             plugin = model
@@ -76,10 +80,15 @@ class ModelVote(object):
             probs = rowwise_indexed(res, cats)
             cats, probs = map(lambda x: x.reshape(-1, 1, x.shape[1]), (cats, probs))
 
-            cats = plugin.sub_cats(cats)
+            conv_vec = np.arange(res.shape[1]).reshape(1, -1)
+            conv_vec = plugin.sub_cats(conv_vec)
+
             if self.class_maps and self.class_maps[i]:
                 for ind, clas in enumerate(self.class_maps[i]):
-                    cats[cats == ind] = clas
+                    conv_vec[conv_vec == ind] = clas
+
+            if ind, val in enumerate(conv_vec):
+                cats[cats == ind] = val
 
             votes = np.concatenate((cats, probs), axis=1)
             votes = np.transpose(votes, (0, 2, 1))
@@ -88,8 +97,11 @@ class ModelVote(object):
             res = res.reshape(-1, 1, *res.shape[1:])
             all_votes.append(votes)
             all_probs.append(res)
+            all_convs.append(conv_vec)
 
         res_mat = np.concatenate(all_votes, axis=1)
+        conv_mat = np.concatenate(all_convs)
         probs = np.concatenate(all_probs, axis=1)
 
-        return Predictions(res_mat, probs, self)
+        return Predictions(res_mat, probs, conv_mat, self)
+
--- pyvote/utils.py
+++ pyvote/utils.py
@@ -1,4 +1,8 @@
 import numpy as np
+from operator import attrgetter
+
+def iterate_columns(mat):
+	return map(attrgetter('T'), mat.T)
 
 def rowwise_indexed(mat, idx):
 	ran = np.arange(idx.shape[0]).reshape(-1, 1)
@@ -9,3 +13,27 @@ def columnwise_indexed(mat, idx):
 	ran = np.arange(idx.shape[1]).reshape(1, -1)
 	ran = np.repeat(ran, idx.shape[0])
 	return mat[ran, idx]
+
+def row_conv_to_idx(mat, conv):
+	ind_mat = np.zeros(mat.shape)
+	for ind, col in enumerate(iterate_columns(mat)):
+		ind_mat[mat == col] = ind
+	return ind_mat
+
+def row_idx_to_conv(mat, conv):
+	conv_mat = np.zeros(mat.shape)
+	for ind, col in enumerate(iterate_columns(mat)):
+		conv_mat[mat == ind] = col
+	return conv_mat
+
+def col_conv_to_idx(mat, conv):
+	ind_mat = np.zeros(mat.shape)
+	for ind, row in enumerate(mat):
+		ind_mat[mat == row] = ind
+	return ind_mat
+
+def col_idx_to_conv(mat, conv):
+	conv_mat = np.zeros(mat.shape)
+	for ind, row in enumerate(mat):
+		conv_mat[mat == ind] = row
+	return conv_mat
