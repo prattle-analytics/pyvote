db15f149313f81138efae9d48de16ae7cfa5dccd
cfeenstra67 <cameron.l.feenstra@gmail.com>
2018-06-06 00:29:43 -0500

    better label handling and added unit tests

--- pyvote/utils.py
+++ pyvote/utils.py
@@ -6,6 +6,14 @@ def rowwise_indexed(mat, idx):
 	return mat[ran, idx]
 
 def columnwise_indexed(mat, idx):
-	ran = np.arange(idx.shape[1]).reshape(1, -1)
-	ran = np.repeat(ran, idx.shape[0])
-	return mat[ran, idx]
+	return rowwise_indexed(mat.T, idx.T).T
+
+def sub_cats(mat, cats, reverse=False):
+	mat_bak = mat.copy()
+	for ind, cat in enumerate(cats):
+		if reverse:
+			mat[mat_bak == cat] = ind
+		else:
+			mat[mat_bak == ind] = cat
+	del mat_bak
+	return mat
